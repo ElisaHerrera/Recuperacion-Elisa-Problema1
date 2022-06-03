0a12dc5df25a04b430c11b233b75f876d3430e5f
ElisaHerrera <102582595+ElisaHerrera@users.noreply.github.com>
2022-06-03 13:12:55 +0100

    Añadido clase Documento Ofimatico y impresora

---
+++ src/DocumentoOfimatico.java
@@ -0,0 +1,30 @@
+public class DocumentoOfimatico {
+    private String nombre;
+    private Tamano tamano;
+    private static enum Tamano{
+        TAMANO,
+        UNIDAD
+    }
+
+    public DocumentoOfimatico(String nombre, Tamano tamano) {
+        this.nombre = nombre;
+        this.tamano = tamano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Tamano getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(Tamano tamano) {
+        this.tamano = tamano;
+    }
+}
+
---
+++ src/Impresora.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Impresora {
+    public static enum Estado{
+        APAGADO,
+        ENCEDIDO
+    }
+    public class ColadeImpresion{
+        private ArrayList<String> lista = new ArrayList<String>();
+
+
+    }
+}
